790ec2a867b4a26279d4b8ec1267134e4f443bfc
ussivan <ivan.uss.2000@gmail.com>
2019-06-15 13:25:04 +0300

    add huffman

---
+++ cmake-build-debug/CMakeFiles/huffman.lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/huffman.lib.dir/huffman.cpp.o"
+  "libhuffman.lib.pdb"
+  "libhuffman.lib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/huffman.lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
